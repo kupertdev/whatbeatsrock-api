1403ccc0a7cc267c516871cf8043b30a0b22fe96
kupertdev <realalise1@gmail.com>
2024-08-30 16:31:20 +0300

    Update game_request_custom.py

--- .code-snippets/game_requests/game_request_custom.py
+++ .code-snippets/game_requests/game_request_custom.py
@@ -1,17 +1,13 @@
 from whatbeatsrock.api.game import Game
 
-import asyncio
-
 async def main():
     game = Game(username="top")
 
-    user_id = await game.get_user_id()
+    user_id = game.get_user_id()
 
     if not user_id:
         raise ValueError("Failed to get user ID.")
                 
     # game.memory_storage.set("room_id", user_id)
 
-    await game.game_request("default custom word", "guess word", user_id)
-
-asyncio.run(main())
+    game.game_request("default custom word", "guess word", user_id)
