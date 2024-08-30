0670c8c88fee1c9809e4c38e6fc1e65ab539509e
kupertdev <realalise1@gmail.com>
2024-08-30 16:31:02 +0300

    Update game_request.py

--- .code-snippets/game_requests/game_request.py
+++ .code-snippets/game_requests/game_request.py
@@ -1,14 +1,9 @@
 from whatbeatsrock.api.game import Game
 from whatbeatsrock.utils.id_generator.generators import IDGenerator
 
-import asyncio
+def main():
 
-async def main():
-
-    uuid_bytes = IDGenerator.generate_uuid_bytes()
-    gid = IDGenerator.format_uuid_bytes_to_string(uuid_bytes)
+    gid = IDGenerator.generate_uuid_v4()
 
     game = Game()
-    await game.game_request("rock", "scissors", gid)
-
-asyncio.run(main())
+    game.game_request("rock", "scissors", gid)
